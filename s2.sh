aba3b0e8a96d90ecf42c1efebac05410d1de7247
Jonathan Tito <jotitoon@uide.edu.ec>
2026-07-10 00:57:22 +0000

    wip: s2

---
+++ s2.sh
@@ -0,0 +1,21 @@
+# ssh .......................
+ssh bandit0@bandit.labs.overthewire.org -p 2220
+# limpia la pantalla de la terminal
+clear
+# me da el historial de comandos ejecutados
+history
+#print working directory
+pwd
+#lista archivos y directorios
+ls
+#
+ls --all
+#
+ls -a
+#archivos y directorios q empiezan con punto, son ocultos
+cd . #simboliza el directorio actual
+cd .. #simboliza la carpeta contenedora del directorio actual
+# nos movemos al directorio root que contiene todos los directorios y archivos de linux
+cd /
+#
+ls -a /workspaces/60048-INTRODUCCION-UNIX-2-IIA-2A/carpeta/carpeta_hija
